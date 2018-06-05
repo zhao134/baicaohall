9700b3d9f76dcacbb40846b7a755b93806bd2bbe
zhao134 <1363808304@qq>
2018-06-05 18:37:55 +0800

    okk

---
+++ app/src/main/java/com/example/administrator/baicaohall/data/login_model/Login_Model.java
@@ -0,0 +1,25 @@
+package com.example.administrator.baicaohall.data.login_model;
+
+
+import com.example.administrator.baicaohall.data.login_contract.Login_Contract;
+
+public class Login_Model implements Login_Contract.Model {
+
+    @Override
+    public void getLoginView(Login_Contract.Send_View_Message view, String number, String word) {
+        if("123".equals(number) && "abc".equals(word)){
+            //为接口赋值
+            view.getString("登录成功");
+        }else{
+            //为接口赋值
+            view.getString("登录失败");
+        }
+    }
+
+    @Override
+    public void getHomeInfo(Login_Contract.HomeSend_View_Message view_message) {
+        view_message.getString("");
+    }
+
+
+}
